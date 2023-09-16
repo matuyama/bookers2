acc180069dbe4cb880fc08543518d90c91479f39
matuyama <143941798+matuyama@users.noreply.github.com>
2023-09-16 14:59:29 +0000

    [add]課題3ページリンク

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
