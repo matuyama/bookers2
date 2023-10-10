d9f3639834392924ed59f284fa14f81d2d101dc3
matuyama <143941798+matuyama@users.noreply.github.com>
2023-10-10 14:44:10 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books, only: [:new, :edit, :show, :index, :create, :update, :destroy]
+  resources :books, only: [:new, :edit, :show, :index, :create, :update, :destroy] do
+    resources :post_comments, only: [:create]
+  end
   resources :users, only: [:edit, :show,:update, :index]
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
